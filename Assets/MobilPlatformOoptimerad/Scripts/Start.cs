da2897b19b4a43d71b1e78cac4af056d8570fc6e
AtsutaneNakasone <jonatan.ansin@yh.nackademin.se>
2024-04-26 22:34:55 +0200

    scene update
    getting closer

--- Assets/MobilPlatformOoptimerad/Scripts/Start.cs
+++ Assets/MobilPlatformOoptimerad/Scripts/Start.cs
@@ -2,20 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class start : MonoBehaviour
 {
-
-
+    public GameObject menu;
+    public GameObject loadingInterface;
+    public Image loadingProgressBar;
 
     List<AsyncOperation> scenesToLoad = new List<AsyncOperation>();
 
     // Start is called before the first frame update
     void Start()
     {
+        ShowLoadingScreen();
         scenesToLoad.Add(SceneManager.LoadSceneAsync("Stage"));
         scenesToLoad.Add(SceneManager.LoadSceneAsync("Scene01", LoadSceneMode.Additive));
+        StartCoroutine(LoadingScreen());
+    }
+
+    public void ShowLoadingScreen()
+    {
+        loadingInterface.SetActive(true);
+    }
+
+    IEnumerator LoadingScreen()
+    {
+        float totalProgress = 0;
+        for (int i = 0; i < scenesToLoad.Count; ++i)
+        {
+            while (!scenesToLoad[i].isDone)
+            {
+                totalProgress += scenesToLoad[i].progress;
+                loadingProgressBar.fillAmount = totalProgress / scenesToLoad.Count;
+                yield return null;
+            }
+        }
     }
 
     // Update is called once per frame
