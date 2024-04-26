3016ae835a954dc784831f846d56f451eef1604e
AtsutaneNakasone <jonatan.ansin@yh.nackademin.se>
2024-04-26 10:27:54 +0200

    minor

--- Assets/MobilPlatformOoptimerad/Scripts/FPSCap.cs
+++ Assets/MobilPlatformOoptimerad/Scripts/FPSCap.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class FPSCap : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] int targetFramerate = 90;
+    //This script unlocks the framerate of the Oculus headset
     void Start()
     {
-        
-    }
+        float[] rates;
+        Unity.XR.Oculus.Performance.TryGetAvailableDisplayRefreshRates(out rates);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Unity.XR.Oculus.Performance.TrySetDisplayRefreshRate(targetFramerate);
+
+        float rate;
+        Unity.XR.Oculus.Performance.TryGetDisplayRefreshRate(out rate);
     }
 }
